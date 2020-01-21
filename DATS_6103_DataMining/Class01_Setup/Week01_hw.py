c69acceb222f44f81d180fb757e512909dcc6142
Jingjooz <54562554+Jingjooz@users.noreply.github.com>
2020-01-20 20:43:50 -0500

    Update Week01_hw.py

--- DATS_6103_DataMining/Class01_Setup/Week01_hw.py
+++ DATS_6103_DataMining/Class01_Setup/Week01_hw.py
@@ -15,11 +15,22 @@
 #
 # You can find some cheatsheets to do other basic stuff like bold-face, italicize, tables, etc.
 
-#%%
+#%%[markdown]
 # Question 1: Create a Markdown cell with the followings:
 # Two paragraphs about yourself. In one of the paragraphs, give a hyperlink of a website 
 # that you want us to see. Can be about yourself, or something you like.
 
+# # About myself
+#
+# I am Sanhanat Satetasakdasiri. 
+# I come from Bangkok, Thailand. 
+# I am studing MA in Economics and Graduate Certificate in Data Science programs
+# I plan to complete both programs in spring 2020
+#
+# Before I came Washingtion D.C., I worked as Economist at Ministry of Finance of Thailand.
+# I took responbility to monitor Thai economic situation.
+# Then, I achieved the Royal Thai government scholarship for master degree. That is why I am here.
+# please contact me at [link](https://www.facebook.com/Jingjooz).
 
 #%%
 # Question 2: Follow our InClass01.py python file, create
@@ -27,10 +38,14 @@
 # Have at least 6 classes, even if you are not a DS major
 # Then print out the last entry in your list.
 
+# list / array
+alist = [1,'Introduction to Data Science',1,'Data Warehousing',1,'Introduction to Data Mining',1,'Data Science Capstone',1,'Machine Learning I: Algorithm Analysis',1,'Visualization of Complex Data']
+print (alist[-1])
 
 #%%
 # Question 3: After you completed question 2, you feel Intro to data mining is too stupid, so you are going 
 # to replace it with Intro to Coal mining. Do that in python here.
+
 
 
 #%%
